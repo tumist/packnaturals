98ba5472445dd1fb12c305a23a459389262614d5
Tumi Steingrímsson <tumi@mbl.is>
2013-12-16 18:10:24 +0000

    Renamed variables, functions in an attempt to be more self-descriptive
    Removed maybe functor of unpack return value. Using fromJust, input
    outside of function domain will now raise Exception: Maybe.fromJust: Nothing

--- packnaturals.hs
+++ packnaturals.hs
@@ -1,44 +1,43 @@
 import Data.List (elemIndex, sort)
 import System.Environment (getArgs)
+import Data.Maybe (fromJust)
 
 abc = "hdOBCH8Rz~926xKW_vLAwl0Ey3aYpUPkqZ7Q4fVjFgJcXDbTum1SteInGriMs5oN"
 
 pack :: Integral i => [i] -> String
-pack n = concatMap packInt n
+pack n = concatMap packNum n
   where
-    packInt n
+    packNum n
+        | n < 0     = error $ "Can't pack negative numbers"
         | n < 2^5   = [abc !! fromIntegral (n + 2^5)]
-        | otherwise = abc !! fromIntegral (n `mod` 2^5) : packInt (n `div` 2^5)
+        | otherwise = abc !! fromIntegral (n `mod` 2^5) : packNum (n `div` 2^5)
 
-unpack :: String -> Maybe [Integer]
-unpack s = case mapped of Just ns -> Just $ map unpackInteger $ groupByTerminator ns
-                          Nothing -> Nothing
+unpack :: String -> [Integer]
+unpack s = map zipGroup $ groups (fromJust mapped)
   where
     mapped = mapM (\e -> elemIndex e abc) s
-    unpackInteger g = sum $ zipWith f g [0,5..]
-      where
-        f n pow = 2^pow * fromIntegral n :: Integer
-    groupByTerminator [] = []
-    groupByTerminator numlist = num : groupByTerminator rest
+    zipGroup g = sum $ zipWith (\n p -> 2^p * toInteger n) g [0,5..]
+    groups [] = []
+    groups ns = num : groups rest
       where
         -- span splits the list such that fst does not include the
         -- matched element, but the matched element is a part of the number
         -- that we're extracting.
         num = fst spanned ++ [head (snd spanned) - 2^5]
         rest = tail $ snd spanned
-        spanned = span (<2^5) numlist
+        spanned = span (<2^5) ns
 
-deltify il = head il : zipWith subtract il (tail il)
-undeltify = tail . scanl (+) 0
 packUnordered = pack . deltify . sort
-unpackUnordered l = fmap undeltify $ unpack l
+  where
+    deltify il = head il : zipWith subtract il (tail il)
+unpackUnordered l = undeltify $ unpack l
+  where
+    undeltify = tail . scanl (+) 0
 
 main = do
   args <- getArgs
-  let intArgs = map (\x -> read x :: Int) args
+  let intArgs = map (\x -> read x :: Integer) args
   let packed = pack intArgs
   let unpacked = unpack packed
   putStrLn $ "packed   : " ++ packed
-  putStr $ "unpacked : "
-  case unpacked of Nothing   -> putStrLn "ERROR"
-                   Just nums -> putStrLn $ show nums
+  putStrLn $ "unpacked : " ++ show unpacked
